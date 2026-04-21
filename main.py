ea0a11cac0774b5c37f8cbc4440c08a7f94fc5fb
khbash <khrystynabash09@gmail.com>
2026-04-21 15:40:42 +0300

    bubble

--- main.py
+++ main.py
@@ -15,3 +15,10 @@ for i in range(len(a)-1):
     ind = a.index(m)
     a[i], a[ind] = a[ind], a[i]
     print(a)
+
+for i in range(0, len(a)-1):
+    for j in range(0, len(a)-1-i):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+
+print(a)
